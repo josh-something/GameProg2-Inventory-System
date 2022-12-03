1f9975630eeee8e39ee587cbceb615bf0dc5c260
Joshua Franz Sabaot <joshuafranzsabaot@gmail.com>
2022-12-04 01:22:30 +0800

    Refactor(Item Database): add Namespace "GameProg2.Items"
    Added the Namespace GameProg2, and subnamespace Items for organizational purposes

--- Assets/Scripts/ItemDatabase/ItemData.cs
+++ Assets/Scripts/ItemDatabase/ItemData.cs
@@ -1,26 +1,30 @@
 using UnityEngine;
 
 
-public enum ItemTypes
+namespace GameProg2.Items
 {
-    Miscellaneous = 0,
-    Equipment = 1,
-    Consumable = 2
-}
-[CreateAssetMenu(menuName = "Item Data", fileName = "New Item")]
-public class ItemData : ScriptableObject
-{
-    [SerializeField] Sprite designatedSprite;
-    [SerializeField] string itemID;
-    [field: TextArea]
-    [SerializeField] string description = "Item Description";
-    [SerializeField] ItemTypes itemType;
-    [SerializeField] int stackSize = 1;
-    
+    public enum ItemTypes
+    {
+        Miscellaneous = 0,
+        Equipment = 1,
+        Consumable = 2
+    }
 
-    public Sprite DesignatedSprite { get => designatedSprite; }
-    public string ItemID { get => itemID; }
-    public string Description { get => description; }
-    public ItemTypes ItemType { get => itemType; }
-    public int StackSize { get => stackSize; }
+    [CreateAssetMenu(menuName = "Item Data", fileName = "New Item")]
+    public class ItemData : ScriptableObject
+    {
+        [SerializeField] Sprite designatedSprite;
+        [SerializeField] string itemID;
+        [field: TextArea]
+        [SerializeField] string description = "Item Description";
+        [SerializeField] ItemTypes itemType;
+        [SerializeField] int stackSize = 1;
+
+
+        public Sprite DesignatedSprite { get => designatedSprite; }
+        public string ItemID { get => itemID; }
+        public string Description { get => description; }
+        public ItemTypes ItemType { get => itemType; }
+        public int StackSize { get => stackSize; }
+    } 
 }
--- Assets/Scripts/ItemDatabase/ItemDatabaseList.cs
+++ Assets/Scripts/ItemDatabase/ItemDatabaseList.cs
@@ -4,28 +4,31 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-public static class ItemDatabaseList
+namespace GameProg2.Items
 {
-    static List<ItemData> _itemList = new();
-    public static List<ItemData> List
+    public static class ItemDatabaseList
     {
-        get { return _itemList; }
-    }
-
-    static ItemDatabaseList()
-    {
-        PopulateList();
-    }
-
-    static void PopulateList()
-    {
-        string[] assetNames = AssetDatabase.FindAssets("t:ItemData", new[] { "Assets/ItemDatabase/ItemData" });
-        _itemList.Clear();
-        foreach (string SOName in assetNames)
+        static List<ItemData> _itemList = new();
+        public static List<ItemData> List
         {
-            var SOpath = AssetDatabase.GUIDToAssetPath(SOName);
-            var itemData = AssetDatabase.LoadAssetAtPath<ItemData>(SOpath);
-            _itemList.Add(itemData);
+            get { return _itemList; }
+        }
+
+        static ItemDatabaseList()
+        {
+            PopulateList();
+        }
+
+        static void PopulateList()
+        {
+            string[] assetNames = AssetDatabase.FindAssets("t:ItemData", new[] { "Assets/ItemDatabase/ItemData" });
+            _itemList.Clear();
+            foreach (string SOName in assetNames)
+            {
+                var SOpath = AssetDatabase.GUIDToAssetPath(SOName);
+                var itemData = AssetDatabase.LoadAssetAtPath<ItemData>(SOpath);
+                _itemList.Add(itemData);
+            }
         }
     }
 }
